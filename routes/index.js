b36be9448fe3c9f72a7458d7b034f3f9c5692cac
Prasanna <prasanna.colan@gmail.com>
2022-01-21 00:14:27 -0800

    update test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express to Test in AWS :-)' });
+  res.render('index', { title: 'Express to Test in AWS :-) updated' });
 });
 
 module.exports = router;
