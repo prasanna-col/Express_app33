eb54113a63049593d2f21b814d03ae6d8777e9fe
Vigneshwar <prasannavenkatesh@colanonline.com>
2022-02-11 10:56:15 +0530

    java file to test in jemkins

---
+++ HelloWord.java
@@ -0,0 +1,11 @@
+class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HaHa -- Hello world");
+    }
+}
+
+// class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!"); 
+//     }
+// }`
